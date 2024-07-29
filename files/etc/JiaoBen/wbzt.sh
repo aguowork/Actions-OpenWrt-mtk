6bc1f21323c6a1288d8d9fec2b8bb87008b8c6de
aguowork <93486858+aguowork@users.noreply.github.com>
2024-07-29 22:08:35 +0800

    Update wbzt.sh

--- files/etc/JiaoBen/wbzt.sh
+++ files/etc/JiaoBen/wbzt.sh
@@ -8,23 +8,29 @@ APP_TOKEN="AT_jf0zuTx0PjA4qBnyCGeKf5J4t0DeUIc6"
 MY_UID="UID_L22PV9Qdjy4q6P3d0dthW1TJiA3k"
 TOPIC_ID="25254"
 
+# 设置错误处理的 trap
+trap 'handle_error' ERR
+
+# 错误处理函数
+handle_error() {
+    # 获取最后一次命令的退出状态
+    local last_exit_status=$?
+    # 获取错误发生的函数名
+    local func_name=${FUNCNAME[1]}  # 获取调用 handle_error 的函数名，通常是发生错误的函数
+    # 获取错误发生的行号
+    local line_number=${BASH_LINENO[0]}
+
+    if [ "$last_exit_status" != "0" ]; then
+        log_message "发生错误：错误发生在函数 $func_name 的第 $line_number 行。已停止脚本执行，请检查脚本！"
+        exit 1
+    fi
+}
+
 # 日志记录函数
 log_message() {
     local msg="$1"
     echo "$(date "+%Y-%m-%d %H:%M:%S") - $msg" | tee -a "${LOG_FILE}"
 }
-
-# 错误处理函数
-handle_error() {
-    # 如果最后的命令退出状态不为零，则认为是错误导致的退出
-    local last_exit_status=$?
-    if [ "$last_exit_status" != "0" ]; then
-        log_message "发生错误，已停止脚本执行，请检查脚本！"
-    fi
-}
-
-# 设置trap，只捕获ERR信号（错误），而不是EXIT（退出）trap 'handle_error' ERR EXIT
-trap 'handle_error' ERR
 
 # 推送消息函数
 push_message() {
@@ -75,7 +81,7 @@ check_internet
 #检查日志文件是否存在，不存在则创建日志文件
 check_log_size
 # 执行 curl 命令，并将输出保存到 json 变量中
-json_data=$(curl -s 'https://weibo.com/ajax/profile/info?uid=123456789' \
+json_data=$(curl -s 'https://weibo.com/ajax/profile/info?uid=3192362522' \
   -H 'user-agent: Mozilla/5.0 (compatible; OpenWrt-Curl/7.76.1)' \
   -H 'cookie: SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9WW8B_YqNSTTLAQjsCyQRNh7; SINAGLOBAL=407691405303.4852.1715057530312; ULV=1717117485200:3:3:1:6705765528571.046.1717117485174:1715134822016; UOR=,,cn.bing.com; WBPSESS=gJ7ElPMf_3q2cdj5JUfmvHwiwgiIZ7iWpDy9pdknT22VoYm35Lxd5oIpJGcEiFRbVf1dHSiQb_nE-YahsBKpEi7cWwoBB-CwIjMbAQH7YHz608OF33JN1SLi1zeI6fhJ; SUB=_2AkMROYwRf8NxqwFRmfsUy2vraYV_wgnEieKnZX3KJRMxHRl-yj9kql4HtRB6Ormi_i_OZvB3XEXW2okwJzfJoxB0r0VS; XSRF-TOKEN=s95Y3NytmKofRfQwiDNehSS9')
 
@@ -95,7 +101,10 @@ IFS='-' read -r -a array <<< "$content"
 
 # 循环判断数值是否变化，若有变化则推送消息
 if [ "$statuses_count" != "${array[0]}" ] || [ "$friends_count" != "${array[1]}" ] || [ "$followers_count_str" != "${array[2]}" ] || [ "$description" != "${array[3]}" ] || [ "$repost_cnt" != "${array[4]}" ] || [ "$comment_cnt" != "${array[5]}" ] || [ "$like_cnt" != "${array[6]}" ]; then
+    # 写入日志文件
     sed -i "s/#Key=\"$content\"/#Key=\"$statuses_count-$friends_count-$followers_count_str-$description-$repost_cnt-$comment_cnt-$like_cnt\"/g" "${LOG_FILE}"
+    # 推送消息
     log_message "NRGX NRS：$statuses_count GZS：$friends_count FSS：$followers_count_str GRJJ：$description LJZFL：$repost_cnt LLPLL：$comment_cnt LJHZ：$like_cnt  GXQ NRS：${array[0]} GZS：${array[1]} FSS：${array[2]} GRJJ：${array[3]} LJZFL：${array[4]} LLPLL：${array[5]} LJHZ：${array[6]} "
+    # 推送消息
     push_message "NRGX\nNRS：$statuses_count\nGZS：$friends_count\nFSS：$followers_count_str\nGRJJ：$description\nLJZFL：$repost_cnt \nLLPLL：$comment_cnt \nLJHZ：$like_cnt  \n\nGXQ\nNRS：${array[0]}\nGZS：${array[1]}\nFSS：${array[2]}\nGRJJ：${array[3]} \nLJZFL：${array[4]} \nLLPLL：${array[5]} \nLJHZ：${array[6]}"
 fi
